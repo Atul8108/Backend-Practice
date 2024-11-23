533fc41e6d86e9367e9ee6757557b4baa6b2b535
Atul Shaw <shawatul4@gmail.com>
2024-11-23 23:16:18 +0530

    create user Router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandlers";
 import createHttpError from "http-errors";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -12,8 +13,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
 });
 
-
-
+//user routes
+app.use('/api/users',userRouter)
 
 //global Error Hndler
 app.use(globalErrorHandler);
